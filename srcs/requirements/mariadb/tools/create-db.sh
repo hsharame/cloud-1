2020a4cff8b2f37c62971e64414bbe441d0c4159
Hanna <sharametahanna@gmail.com>
2026-03-11 14:35:26 +0000

    feat(tls): added openssl cert

--- srcs/requirements/mariadb/tools/create-db.sh
+++ srcs/requirements/mariadb/tools/create-db.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
+set -e
 
-#service mysql start
-
-#arreter en cas d'erreur
-set -e 
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+chmod 750 /var/lib/mysql
+chmod 755 /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	chown -R mysql:mysql /var/lib/mysql
-	chmod 750 /var/lib/mysql
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
 fi
 
-if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
+mysqld --user=mysql --skip-networking --socket=/run/mysqld/mysqld.sock &
+pid="$!"
 
-    cat << EOF > /tmp/create_db.sql
+until mariadb-admin --socket=/run/mysqld/mysqld.sock ping >/dev/null 2>&1; do
+    sleep 1
+done
+
+mariadb --socket=/run/mysqld/mysqld.sock <<EOF
 USE mysql;
-FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-CREATE DATABASE \`${SQL_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+ALTER USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';
+
 FLUSH PRIVILEGES;
 EOF
 
-    mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-    rm -f /tmp/create_db.sql
+mariadb-admin --socket=/run/mysqld/mysqld.sock -uroot -p"${SQL_ROOT_PASSWORD}" shutdown
+wait "$pid" 2>/dev/null || true
 
-fi
-
-exec mysqld --user=mysql
+exec mysqld --user=mysql --console
